c877e2449e2fedcf4bd3c3e0c7703f4e26fb4a25
Peter Ayeni <ayenicology@gmail.com>
2021-02-24 10:34:49 +0000

    chore: implement configurations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const port = 3000;
+
+  const serverConfig = config.get('server');
+  const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
